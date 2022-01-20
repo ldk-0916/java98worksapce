fe925d21261ed1e6b8b2f0af182ac2b2d63e968d
ldk <134551905@qq.com>
2022-01-20 15:44:40 +0800

    新创建了一个UserServlet

---
+++ Web07_Git/src/main/java/com/yys/web/UserServlet.java
@@ -0,0 +1,11 @@
+package com.yys.web;
+
+/**
+ * @Author: 夜遊神
+ * @Date: 2022/01/20/15:42
+ */
+public class UserServlet {
+    public static void main(String[] args) {
+        System.out.println("法外狂徒张三");
+    }
+}
